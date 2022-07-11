4ef14c195a22d9e09e40790a7ea8e44fea6a213a
JotaVPinheiro <jotavpinheiro@gmail.com>
2022-07-11 13:13:11 -0300

    Organize files and update extension popup

--- src/background.js
+++ src/background.js
@@ -1,7 +1,7 @@
 chrome.tabs.onActivated.addListener((tab) => {
   chrome.tabs.get(tab.tabId, (currentTabInfo) => {
     if (currentTabInfo.url == "https://web.whatsapp.com/") {
-      chrome.tabs.executeScript(null, { file: "./foreground.js" }, () => {
+      chrome.tabs.executeScript(null, { file: "./src/foreground.js" }, () => {
         console.log("Foreground script executed");
       });
     }
