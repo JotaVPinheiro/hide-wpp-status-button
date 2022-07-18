2141075e5ce8c5a09b748908e55c847cca399bb3
JotaVPinheiro <jotavpinheiro@gmail.com>
2022-07-18 11:51:32 -0300

    Remove script execution on background.js

--- src/background.js
+++
@@ -1,9 +0,0 @@
-chrome.tabs.onActivated.addListener((tab) => {
-  chrome.tabs.get(tab.tabId, (currentTabInfo) => {
-    if (currentTabInfo.url == "https://web.whatsapp.com/") {
-      chrome.tabs.executeScript(null, { file: "./src/foreground.js" }, () => {
-        console.log("Foreground script executed");
-      });
-    }
-  });
-});
